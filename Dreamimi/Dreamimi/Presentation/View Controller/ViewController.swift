a1681adc78cfe3ab5267a37eb0333b7b01d27861
Sergey Germanovich <sergey_chip@tut.by>
2018-10-22 18:09:15 +0300

    initial commit

---
+++ Dreamimi/Dreamimi/Presentation/View Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Dreamimi
+//
+//  Created by Sergey Germanovich on 15/10/2018.
+//  Copyright © 2018 Redmadrobot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private weak var flashButton: UIButton!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func flashTapped(_ sender: Any) {
+        // Инициализируем генератор
+        let generator = UINotificationFeedbackGenerator()
+        // Вызываем taptic feedback с типом success
+        generator.notificationOccurred(.success)
+    }
+    
+}
+
