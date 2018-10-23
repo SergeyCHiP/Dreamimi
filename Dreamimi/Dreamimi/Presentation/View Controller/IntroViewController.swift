f974d6f203890f1c4cc3e9591ffea3440bdc32d4
Sergey Germanovich <sergey_chip@tut.by>
2018-10-23 18:09:39 +0300

    Добавил параллакс и таптик

--- Dreamimi/Dreamimi/Presentation/View Controller/IntroViewController.swift
+++ Dreamimi/Dreamimi/Presentation/View Controller/IntroViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 final class IntroViewController: UIViewController {
     
+    @IBOutlet weak var bigRobot: UIImageView!
     @IBAction func nextTapped(_ sender: Any) {
         let viewController = UIStoryboard(name: "Main", bundle: nil)
             .instantiateViewController(withIdentifier: "MainViewController")
         viewController.modalTransitionStyle = .crossDissolve
         show(viewController, sender: nil)
+        // Инициализируем генератор
+        let generator = UINotificationFeedbackGenerator()
+        // Вызываем taptic feedback с типом success
+        generator.notificationOccurred(.success)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bigRobot.addParallax(withValue: 40)
     }
 }
